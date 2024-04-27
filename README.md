84e6d892c7343ea7eee43a85c5adce7db3ffe4c3
Srinivasara_dheekonda <vasuvasu.srinu1996@gmail.com>
2024-04-27 13:56:15 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # HateSpeachNLP
+
+## how to run
+
+```bash
+conda create -n hate python==3.11.5 -y
+```
+```bash
+conda activate hate
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup (
+    name = "HateSpeachNLP",
+    version = "0.0.1",
+    author = "srinivas-dheekonda",
+    author_email = "vasuvasu.srinu1996@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
