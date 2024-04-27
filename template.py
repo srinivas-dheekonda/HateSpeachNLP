efadfbebee899a396823ff9946d80363ddad16bb
Srinivasara_dheekonda <vasuvasu.srinu1996@gmail.com>
2024-04-27 12:27:03 +0530

    Folder structure added

---
+++ template.py
@@ -0,0 +1,58 @@
+# Logging string
+import os
+from pathlib import Path
+import logging 
+
+logging.basicConfig(level = logging.INFO,format ='[%(asctime)s]:%(message)s')
+
+project_name = "HateSpeech"
+
+list_of_files = [
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/configuration/GCP_syncer.py",
+    f"{project_name}/constants/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/ml/__init__.py",
+    f"{project_name}/ml/model.py",
+    "main.py",
+    "app.py",
+    "demo.py",
+    "requirements.txt",
+    "dockerfile",
+    "setup.file",
+    ".dockerignore",
+    "flowchart"
+
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"creating directorry;{filedir}; for the file; {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"creating empty file:{filename}")
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
